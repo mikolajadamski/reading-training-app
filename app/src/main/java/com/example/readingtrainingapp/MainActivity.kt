cd8143660f0142d572fa7c18329dfe86be5cf4c7
mikolajadamski <mikolaj.adamski47@gmail.com>
2021-12-15 00:12:54 +0100

    dodanie reklam i klawiszy

--- app/src/main/java/com/example/readingtrainingapp/MainActivity.kt
+++ app/src/main/java/com/example/readingtrainingapp/MainActivity.kt
@@ -9,17 +9,22 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import java.util.*
+import java.util.Collections.singletonList
 
 class MainActivity : AppCompatActivity() {
 
     private val visibilityTime: Long = 2000
 
-    private val SHOW_NUMBER_TEXT: String =  getString(R.string.show_number)
+    private val SHOW_NUMBER_TEXT: String = "Pokaż"
 
-    private val CHECK_NUMBER_TEXT: String =  getString(R.string.check_number)
+    private val CHECK_NUMBER_TEXT: String =  "Sprawdź"
 
-    private val NEXT_TEXT: String =  getString(R.string.next)
+    private val NEXT_TEXT: String =  "Następny"
 
     private lateinit var gamePhase: Phase
 
@@ -27,11 +32,22 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var textToRemember: TextView
 
-    private lateinit var inputNumber: EditText
+    private lateinit var inputNumber: TextView
+
+    private lateinit var mAdView : AdView
+
+    private var digitButtons: List<Int> = listOf(R.id._0, R.id._1, R.id._2, R.id._3, R.id._4, R.id._5, R.id._6,
+        R.id._7, R.id._8, R.id._9)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this)
+
+        mAdView = findViewById(R.id.adView)
+
+        mAdView.loadAd(AdRequest.Builder().build());
+
         readCheckButton = findViewById(R.id.read_check_button)
         inputNumber = findViewById(R.id.input_number_edit_text)
         gamePhase = Phase.READ
@@ -44,6 +60,16 @@ class MainActivity : AppCompatActivity() {
             }
         }
         textToRemember = findViewById(R.id.text_to_remember_textview)
+
+        for(id in digitButtons.indices) {
+            findViewById<Button>(digitButtons[id]).setOnClickListener {
+                inputNumber.append((id.toString()))
+            }
+        }
+
+        findViewById<Button>(R.id.delete).setOnClickListener {
+            inputNumber.text = inputNumber.text.subSequence(inputNumber.text.lastIndex - 1, inputNumber.text.lastIndex)
+        }
     }
 
     private fun makeVisibleForNMilis(milis: Long) {
@@ -72,7 +98,7 @@ class MainActivity : AppCompatActivity() {
         gamePhase = Phase.DURING_ACTIVITY
         textToRemember.visibility = View.INVISIBLE
         inputNumber.setTextColor(Color.BLACK)
-        inputNumber.text.clear()
+        inputNumber.clearComposingText()
         setButtonText(SHOW_NUMBER_TEXT)
         gamePhase = Phase.READ
     }
